feb6c2d3090ece12e1845658dd5a68dbb48a5109
fred <domaine75@gmail.com>
2023-03-28 00:40:08 +0200

    clean code

--- src/practice-1/FormsMain/JamesViewModel.cs
+++ src/practice-1/FormsMain/JamesViewModel.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FormsMain
+﻿namespace FormsMain
 {
-    public class JamesViewModel
+  public class JamesViewModel
     {
         public string Name { get; set; }
         public bool IsTest { get; set; }
--- src/practice-1/WpfApp1/App.cs
+++ src/practice-1/WpfApp1/App.cs
@@ -1,14 +1,9 @@
 ﻿using FormsMain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace WpfApp1
 {
-    internal class App : Application
+  internal class App : Application
     {
         protected override void OnStartup(StartupEventArgs e)
         {
